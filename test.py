cd264b9f9b6f9782abb9e0d1832dcd891f379bf6
YimvPi <yfg0314@gmail.com>
2024-11-27 23:32:22 -0500

    1127

--- test.py
+++ test.py
@@ -1,15 +1,28 @@
-nums = [1,2,3]
+nums = [0,1]
 
+res = majority = 0
+
+for n in nums:
+    majority = 1 if n == res else -1
+
+majority
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 left = 1
 right = len(nums) - 1
-
-
-
 
 b = sum(nums[left:right+1])
 
 
-
+majority += 1 if n == res else -1
 
 n = len(nums)
 
@@ -18,3 +31,6 @@ res = [0] * 6
 
 if 5+6 >4 and 5-6 < -8:
     print("hello")
+    
+    
+    
