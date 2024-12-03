e0f3898ca7ace140fee406095888d19d1cb720f2
YimvPi <yfg0314@gmail.com>
2024-12-02 23:18:52 -0500

    1202

--- test.py
+++ test.py
@@ -1,17 +1,34 @@
-nums = [0,1]
+
+
+# Empty dictionary
+dict = {}
+
+# Dictionary with initial key-value pairs
+dict = {
+    "name": "Alice",
+    "age": 25,
+    "city": "New York"
+}
+
+
+dict.get("name", 2)  # If "name" exists, it returns the value, otherwise it returns 2
+
+
+
+
+
+
+nums = [0,1,2,3,4,5,6,7,8,9]
+
+nums[1:]
 
 res = majority = 0
+
 
 for n in nums:
     majority = 1 if n == res else -1
 
 majority
-    
-    
-    
-    
-    
-    
     
     
     
