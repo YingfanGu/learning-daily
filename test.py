e1c72d1d956b0f7157dbaae95644bfbdb4654a09
YimvPi <yfg0314@gmail.com>
2025-01-12 19:59:58 -0500

    0112

--- test.py
+++ test.py
@@ -1,91 +1,27 @@
-
-
-
-
-s = "the sky is blue"
-
-for i, x in enumerate(s):
-    print(i, x)
-
-w = reversed(s)
-
-k = reversed(s.split())
-
-result = divmod(-10, 3)
-print(result)  # Output: (-4, 2)
-
-a=10
-b=3
-(a // b, a % b)
-
-
-# Empty dictionary
-dict = {}
-
-# Dictionary with initial key-value pairs
-dict = {
-    "name": "Alice",
-    "age": 25,
-    "city": "New York"
-}
-
-
-dict.get("name", 2)  # If "name" exists, it returns the value, otherwise it returns 2
-
-dict.pop("name")
-
-
-res = 'MMKK'
-k = res[1:3]
-
-
-
-K = "".join(res)  
-
-
-for i in range(5):
-    print(i)
-
-
-
-
-from random import choice
-
-nums = [0,1,2,3,4,5,6,7,8,9]
-
-
-choice(nums)
-
-nums.pop()
-
-nums[1:]
-
-res = majority = 0
-
-
-for n in nums:
-    majority = 1 if n == res else -1
-
-majority
+strs = ["eat"]
+key = sorted(strs)
+print(key)
     
     
     
-    
-left = 1
-right = len(nums) - 1
-
-b = sum(nums[left:right+1])
-
-
-majority += 1 if n == res else -1
-
-n = len(nums)
-
-res = [0] * 6
+for s in strs:
+    print(s)
+    key = sorted(s)
+    print(key)
+            
+            
+key = ''.join(s)
+my_dict = {key: "value"}  # TypeError: unhashable type: 'list'
+my_dict
 
 
-if 5+6 >4 and 5-6 < -8:
-    print("hello")
-    
-    
-    
+("age", 25) in my_dict.items()
+
+
+
+
+my_dict.items()
+
+
+s = "dog cat cat dog"
+words = s.split(' ')
